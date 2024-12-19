afeb347283e6f0d031b472e0d303bd2fd6c6657b
Salida Maharjan <salidamaharjan@outlook.com>
2024-12-18 22:39:16 -0600

    Code refactor

--- src/main/java/stack/overview/Main.java
+++ src/main/java/stack/overview/Main.java
@@ -6,7 +6,8 @@ public class Main {
     public static void main(String[] args) {
         StackOverview languagesLearned = new StackOverview();
         languagesLearned.setLanguages("JavaScript");
-        System.out.println(languagesLearned.getLanguages());
-        
+        languagesLearned.setLanguages("TypeScript");
+        languagesLearned.setLanguages("Java");
+        System.out.println("Languages Learned: " + languagesLearned.getLanguages());
     }
 }
