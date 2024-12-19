fbb46698c2fecbd8b34b9413667a912068fd9f43
Salida Maharjan <salidamaharjan@outlook.com>
2024-12-19 12:58:01 -0600

    Create push method and implement

--- src/main/java/stack/overview/Main.java
+++ src/main/java/stack/overview/Main.java
@@ -1,21 +1,29 @@
 package stack.overview;
 
-public class Main {
-        int maxStore;
-        String[] languages;
+import java.util.Arrays;
 
-    public Main () {
-        maxStore = 5;
-        languages =  new String[maxStore];
-    }
+//push pop length;
+public class Main {
+    int maxStore = 5;
+    String[] languages = new String[maxStore];
+    int top = -1;
 
     public void push(String languageToAdd) {
-        
+        if (top > maxStore) {
+            System.out.println("Cannot add. Reached max limit!");
+        } else {
+            top++;
+            languages[top] = languageToAdd;
+        }
     }
+    
 
     public static void main(String[] args) {
         Main stack = new Main();
         stack.push("Java");
+        stack.push("JavaScript");
+
+        System.out.println("Language Array: " + Arrays.toString(stack.languages));
 
     }
 }
