359612f7960d85add01efefa004eac91ee84d2b9
Salida Maharjan <salidamaharjan@outlook.com>
2024-12-19 07:16:57 -0600

    Create Ask user class'

--- src/main/java/stack/overview/Main.java
+++ src/main/java/stack/overview/Main.java
@@ -1,15 +1,8 @@
 package stack.overview;
 
-import java.util.LinkedList;
-
 public class Main {
     public static void main(String[] args) {
-        StackOverview languagesLearned = new StackOverview();
-        languagesLearned.setLanguages("JavaScript");
-        languagesLearned.setLanguages("TypeScript");
-        languagesLearned.setLanguages("Java");
-        System.out.println("Languages Learned: " + languagesLearned.getLanguages());
-        languagesLearned.removeLanguage();
-        System.out.println("Languages Learned: " + languagesLearned.getLanguages());
+        AskUser ask = new AskUser();
+        ask.askUser();
     }
 }
