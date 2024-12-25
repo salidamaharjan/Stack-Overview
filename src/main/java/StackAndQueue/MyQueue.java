159c2790d545b82edece211d6008b677b22bc1ce
Salida Maharjan <salidamaharjan@outlook.com>
2024-12-24 23:36:50 -0600

    Create class for Queue LinkedList

---
+++ src/main/java/StackAndQueue/MyQueue.java
@@ -0,0 +1,19 @@
+package StackAndQueue;
+
+public class MyQueue implements MyList {
+
+    @Override
+    public void add(String s) {
+        
+    }
+
+    @Override
+    public String remove() {
+        return "";
+    }
+
+    @Override
+    public String peek(int index) {
+        return "";
+    }
+}
