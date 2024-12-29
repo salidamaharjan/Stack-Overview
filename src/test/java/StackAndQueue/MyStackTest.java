cf58488becd9c11de3042b0c6c35d6fb47b0ab4b
Salida Maharjan <salidamaharjan@outlook.com>
2024-12-29 09:09:09 -0600

    Create test for MyList

---
+++ src/test/java/StackAndQueue/MyStackTest.java
@@ -0,0 +1,24 @@
+package StackAndQueue;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyStackTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void peek() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
