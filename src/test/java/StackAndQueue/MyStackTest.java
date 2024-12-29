7c5ea82056d0018a06d5215bb85d4755b41f3df7
Salida Maharjan <salidamaharjan@outlook.com>
2024-12-29 10:19:24 -0600

    Test remove in MyStack

--- src/test/java/StackAndQueue/MyStackTest.java
+++ src/test/java/StackAndQueue/MyStackTest.java
@@ -11,7 +11,7 @@ class MyStackTest {
     @Test
     void add() {
         MyStack newStack = new MyStack();
-       assertEquals(newStack.toString(), "[]");
+        assertEquals(newStack.toString(), "[]");
         newStack.add("S");
         assertNotNull(newStack);
         assertEquals(newStack.peek(0), "S");
@@ -19,6 +19,14 @@ class MyStackTest {
 
     @Test
     void remove() {
+        MyStack newStack = new MyStack();
+        assertEquals(newStack.toString(), "[]");
+        newStack.add("S");
+        newStack.add("A");
+        newStack.add("T");
+        newStack.add("M");
+        assertEquals( newStack.peek(3),newStack.remove());
+        assertEquals(newStack.peek(2),newStack.remove());
     }
 
     @Test
