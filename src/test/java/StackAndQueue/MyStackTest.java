ac4209f28135951139851ec891567f2f8586dfc0
Salida Maharjan <salidamaharjan@outlook.com>
2024-12-29 10:22:26 -0600

    Test peek in MyStack

--- src/test/java/StackAndQueue/MyStackTest.java
+++ src/test/java/StackAndQueue/MyStackTest.java
@@ -25,12 +25,20 @@ class MyStackTest {
         newStack.add("A");
         newStack.add("T");
         newStack.add("M");
-        assertEquals( newStack.peek(3),newStack.remove());
-        assertEquals(newStack.peek(2),newStack.remove());
+        assertEquals(newStack.peek(3), newStack.remove());
+        assertEquals(newStack.peek(2), newStack.remove());
     }
 
     @Test
     void peek() {
+        MyStack newStack = new MyStack();
+        assertEquals(newStack.toString(), "[]");
+        newStack.add("S");
+        newStack.add("A");
+        newStack.add("T");
+        newStack.add("M");
+        assertEquals(newStack.peek(1), "A");
+        assertEquals(newStack.peek(0), "S");
     }
 
     @Test
